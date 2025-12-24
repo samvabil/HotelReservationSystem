edda3d4e558d5a85cdf7e7dff63e7bbdbc4eadab
Samvabi Lamichhane <lamichhanesamvabi@gmail.com>
2025-12-24 12:47:45 -0600

    Created Spring App, Set Up Mongo Connection

---
+++ backend/hotelreservationsystem/src/test/java/com/skillstorm/hotelreservationsystem/HotelreservationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.hotelreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelreservationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
